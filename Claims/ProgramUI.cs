f7b156e7479b63d34625e5695aa64ff4617cceae
Lawrence Wagner <lawrencewagner77@gmail.con>
2019-05-19 20:49:55 -0400

    claims intial

---
+++ Claims/ProgramUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claims
+{
+    class ProgramUI
+    {
+        internal void Run()
+        {
+            SeedData();
+            Console.WriteLine("Welcome to the Claims Handler! Press any key to continue.");
+            Console.ReadKey();
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Clear();
+                MenuPrompt();
+                isRunning = Menu();
+            }
+        }
+
+        private bool Menu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuPrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SeedData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
